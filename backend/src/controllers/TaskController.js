710e5e9580ef7a3f236e6a90aeeca753cc1a1ef6
Anthony <olympnetwork@gmail.com>
2022-11-25 07:42:38 +0100

    feat(BAC-34): Create an endpoint that collects submissions of tasks

--- backend/src/controllers/TaskController.js
+++ backend/src/controllers/TaskController.js
@@ -14,4 +14,20 @@ export default class TaskController {
       next(error);
     }
   }
+
+  static async submitTask(req, res, next) {
+    try {
+      const data = {
+        ...req.body,
+        task_id: req.params.taskId,
+        date_sumitted: new Date().toISOString(),
+        date_updated: '',
+      };
+      console.log(data);
+      const result = await taskService.submitTask(data);
+      return res.send(result?.data);
+    } catch (error) {
+      next(error);
+    }
+  }
 }
--- backend/src/routes/taskRoute.js
+++ backend/src/routes/taskRoute.js
@@ -4,5 +4,6 @@ import TaskController from '../controllers/TaskController.js';
 const router = express.Router();
 
 router.post('/', TaskController.create);
+router.post('/:taskId/submit', TaskController.submitTask);
 
 export default router;
