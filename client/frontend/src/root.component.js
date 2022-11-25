ef01f5b21261c3a839e160fa85ed39ded18e56f0
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2022-11-25 03:10:14 +0100

    Adds modal for tasks Assigned

--- client/frontend/src/root.component.js
+++ client/frontend/src/root.component.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header/Header"
 import TaskHistory from "./components/TaskHistory/TaskHistory"
-import AssignerDeleteTask from "./components/AssignerDeleteTask/AssignerDeleteTask"
+// import AssignerDeleteTask from "./components/AssignerDeleteTask/AssignerDeleteTask"
+import AssignerTaskAssigned from "./components/AssignerTaskAssigned/AssignerTaskAssigned"
 
 export default function Root() {
   return (
@@ -8,7 +9,8 @@ export default function Root() {
       <Header />
       <TaskHistory />
       {/* The below AssignerDeleteTask should be moved into the Assigner/ Candidate Task Assigned page and be toggled from there and closed with prop close_modal */}
-      <AssignerDeleteTask />
+      {/* <AssignerDeleteTask /> */}
+      <AssignerTaskAssigned />
     </>
   )
 }
