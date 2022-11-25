7348d23101d0f46120171b8c30711abadd307f36
SeyiPerfect1 <adeegbeoluseyi@.com>
2022-11-25 05:49:26 +0100

    (feat(BE-5): added all routes

--- backend/src/routes/taskRoute.js
+++ backend/src/routes/taskRoute.js
@@ -4,5 +4,8 @@ import TaskController from '../controllers/TaskController.js';
 const router = express.Router();
 
 router.post('/', TaskController.create);
+router.get('/', TaskController.getAllTask);
+router.get('/user_id', TaskController.getTaskByMe);
+router.post('/category', TaskController.createTaskCategory);
 
 export default router;
