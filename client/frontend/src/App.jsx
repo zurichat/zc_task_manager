1644f293b5fa730f408eeb0e2261e8657cf01875
osuji chiaka <chiakajunior@yahoo.com>
2022-11-25 10:17:55 +0300

    completed the ui design for the add task page

--- client/frontend/src/App.jsx
+++ client/frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom"
 import Header from "./components/Header/Header"
 import TaskAsignedPage from "./components/TaskAsigned/TaskAsigned"
+import AddTask from "./Pages/AddTask/AddTask"
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Header />
       <Routes>
         <Route path="/" element={<TaskAsignedPage />} />
+        <Route path="/addtask" element={<AddTask />} />
         <Route path="/notification" element={<div>Notification</div>} />
       </Routes>
     </>
