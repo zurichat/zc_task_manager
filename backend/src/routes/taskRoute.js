a43b8921f4e6b1da8677548e99901396b62e8571
SeyiPerfect1 <adeegbeoluseyi@.com>
2022-11-25 19:33:46 +0100

    fix(BE): fixed axios get request issue

--- backend/src/routes/taskRoute.js
+++ backend/src/routes/taskRoute.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.post('/', TaskController.create);
 router.get('/', TaskController.getAllTask);
-router.get('/user_id', TaskController.getTaskByMe);
+router.get('/:user_id', TaskController.getTaskByMe);
 router.post('/category', TaskController.createTaskCategory);
 router.post('/:task_id/assign', TaskController.assign);
 router.patch('/:assigned_task_id/reassign', TaskController.reassign);
