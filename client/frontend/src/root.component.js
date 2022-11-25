60b106404cb3c19bfc41e4acb25ddfc45766e3df
mohayax <mohayax02@gmail.com>
2022-11-25 01:00:55 +0100

    tasktable/admin

--- client/frontend/src/root.component.js
+++ client/frontend/src/root.component.js
@@ -1,11 +1,13 @@
 import Header from "./components/Header/Header";
 import TaskHistory from './components/TaskHistory/TaskHistory'
+import TaskTable from './components/TaskTable/TaskTable';
 
 export default function Root() {
   return (
     <>
       <Header />
       <TaskHistory />
+      <TaskTable />
     </>
   )
 }
