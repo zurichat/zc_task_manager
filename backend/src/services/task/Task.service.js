6e8579a3f4b0ceefa13ccbfc2ab8d085861c64f8
SeyiPerfect1 <adeegbeoluseyi@.com>
2022-11-25 05:50:17 +0100

    (feat(BE-5): added createTaskCategory service

--- backend/src/services/task/Task.service.js
+++ backend/src/services/task/Task.service.js
@@ -11,6 +11,26 @@ class TaskService extends HttpRepo {
 
     return create;
   }
+
+  async getAllTasks() {
+    const get = await this.repo.findAll();
+    if (!get) throw new NotFoundError("An error occured while fetching tasks");
+
+    return get;
+  }
+
+  async getTaskByMe(params) {
+    const get = await this.repo.findWithFilter(params);
+    if (!get) throw new NotFoundError("An error occured while fetching tasks");
+
+    return get;
+  }
+
+  async createTaskCategory(params) {
+    const create = await this.repo.create(params);
+
+    return create;
+  }
 }
 
 export default new TaskService();
