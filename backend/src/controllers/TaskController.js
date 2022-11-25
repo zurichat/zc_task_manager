e19cd0e92dac65d4d2808d0f93d282ffa5ccc0e5
SeyiPerfect1 <adeegbeoluseyi@.com>
2022-11-25 05:50:48 +0100

    (feat(BE-5): implemented create category endppoint

--- backend/src/controllers/TaskController.js
+++ backend/src/controllers/TaskController.js
@@ -1,5 +1,3 @@
-// import TaskService from '../ser/vices/task/Task.service.js';
-
 import taskService from '../services/task/Task.service.js';
 
 export default class TaskController {
@@ -14,4 +12,52 @@ export default class TaskController {
       next(error);
     }
   }
+
+  static async getAllTask(req, res, next) {
+    let query = {};
+    for (let queryParameter of Object.keys(req.query)) {
+        if (queryParameter === "limit" || 
+        queryParameter === "page");
+        query[queryParameter] = req.query[queryParameter];
+    }
+
+try {
+  // Request validation required
+  const result = await taskService.getAllTasks();
+  console.log(result)
+  const data = new APIFeatures(result, query).paginate();
+
+  res.send({
+    currentPage: `${data[0]}`,
+    noOfPages: `${data[2]}`,
+    data: `${data[3]}`
+  });
+} catch (error) {
+  next(error);
+}
+}
+
+static async getTaskByMe(req, res, next) {
+try {
+  // Request validation required
+  const result = await taskService.getTaskByMe();
+
+  res.send({
+    ...result,
+  });
+} catch (error) {
+  next(error);
+}
+}
+
+static async createTaskCategory(req, res, next) {
+try {
+  // Request validation required
+  const result = await taskService.createTaskCategory(req.body);
+
+  return res.send(result?.data  );
+} catch (error) {
+  next(error);
+}
+}
 }
