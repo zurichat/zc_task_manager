e40b220cc7726a28edd2a91b5b9ad628369b0d59
Anthony <olympnetwork@gmail.com>
2022-12-10 03:38:18 +0100

    refactor: refactor endpoints to adapt with the recent changes on zuricore_main repo

--- backend/src/services/task/Task.service.js
+++ backend/src/services/task/Task.service.js
@@ -1,5 +1,7 @@
 import HttpRepo from '../../database/repositories/HttpRepo.js';
 
+import { sub_collections } from '../../utils/collectionNames.js';
+
 class TaskService extends HttpRepo {
   constructor() {
     super();
@@ -9,13 +11,13 @@ class TaskService extends HttpRepo {
   async create(params) {
     const { organization_id, ...required } = params;
 
-    this.request = { ...this.request, collection_name: 'task', organization_id };
-
     const payload = {
       ...required,
       created_at: new Date().toISOString(),
-      updated_at: '',
+      date_updated: '',
       task_status: 0,
+      sub_collection: sub_collections.tasks,
+      organization_id,
     };
 
     const create = await this.repo.create(payload);
@@ -24,53 +26,98 @@ class TaskService extends HttpRepo {
   }
 
   async update(params) {
-    const {objectId} = params
-    const updatedTask = await this.repo.update(objectId, params);
+    const { object_id, organization_id, ...required } = params;
+    const updatedTask = await this.repo.update({
+      object_id,
+      organization_id,
+      date_updated: new Date().toISOString(),
+      sub_collection: sub_collections.tasks,
+      ...required,
+    });
     return updatedTask;
   }
 
-  async getTasksByCategory(category) {
-    const result = await this.repo.findWhere({task_category: category});
+  async getTasksByCategory(query) {
+    const { organization_id, ...filter } = query;
+    const result = await this.repo.findWhere(organization_id, {
+      ...filter,
+      sub_collection: sub_collections.tasks,
+    });
     return result;
   }
-  
-  async assign(id, params) {
-    const collectionName = 'assigned_tasks';
-    this.request = { ...this.request, collection_name: collectionName };
 
-    const assign = await this.repo.store(id, params);
+  async assign(params) {
+    const assign = await this.repo.create({ ...params, sub_collection: sub_collections.assigned_tasks });
 
     return assign;
   }
 
   async reassign(id, params) {
-    const reassign = await this.repo.store(id, params);
+    const reassign = await this.repo.store({ ...params, object_id: id });
 
     return reassign;
   }
 
   async getAllTasks(organizationId) {
-    const get = await this.repo.findAll(organizationId);
+    const get = await this.repo.findAll(organizationId, { sub_collection: sub_collections.tasks });
     if (!get) throw new NotFoundError('An error occured while fetching tasks');
+
+    const categories = (await this.getAllCategories(organizationId)).data?.data;
+
+    const taskResult = get.data?.data;
+
+    const tasks = [];
+
+    if (!categories) {
+      return taskResult;
+    }
+
+    taskResult.forEach((task) => {
+      const category = categories.find((cat) => cat._id === task.task_category);
+
+      tasks.push({ ...task, category_name: category?.category_name });
+    });
+
+    return tasks;
+  }
+
+  async getAllCategories(organizationId) {
+    const get = await this.repo.findAll(organizationId, { sub_collection: sub_collections.categories });
+    if (!get) throw new NotFoundError('An error occured while fetching categories');
 
     return get;
   }
 
   async getTaskByMe(params) {
-    const get = await this.repo.findWhere(params);
+    const { organization_id, ...filter } = params;
+    const get = await this.repo.findWhere(organization_id, filter);
     if (!get) throw new NotFoundError('An error occured while fetching tasks');
 
     return get;
   }
 
   async createTaskCategory(params) {
-    const create = await this.repo.create(params);
+    const { organization_id, ...required } = params;
+
+    const payload = {
+      ...required,
+      created_at: new Date().toISOString(),
+      updated_at: '',
+      sub_collection: sub_collections.categories,
+      organization_id,
+    };
+    const create = await this.repo.create(payload);
     return create;
   }
 
   async remove(params) {
-    const { objectId } = params;
-    const remove = await this.repo.delete(objectId);
+    // const { object_id, organization_id } = params;
+
+    const remove = await this.repo.delete({
+      ...params,
+      bulk_delete: false,
+      filter: { sub_collection: sub_collections.tasks },
+    });
     return remove.data;
   }
 
@@ -78,34 +125,76 @@ class TaskService extends HttpRepo {
     const submitted = await this.repo.findSubmitted();
     return submitted;
   }
-  async history() {
-    const result = await this.repo.findAll();
-    const submitted = await this.getSubmittedTask()?.data;
-    const tasks = result?.data;
-    const history = tasks.filter((task) => {
-      submitted.some((obj) => {
-        return task._id === obj.task_id;
+  async history(organization_id) {
+    const tasks = await this.getAllTasks(organization_id);
+
+    const submittedTasks = await this.repo.findWhere(organization_id, {
+      sub_collection: sub_collections.submissions,
+    });
+    const submissions = submittedTasks?.data?.data;
+
+    const tasksAndSubmissions = [];
+
+    submissions.forEach((sub) => {
+      const task = tasks.find((task) => task._id === sub.task_id);
+
+      tasksAndSubmissions.push({
+        submission: { ...sub },
+        task: { ...task },
       });
     });
-    return history;
+
+    // const history = tasks.filter((task) => {
+    //   submitted.some((obj) => {
+    //     return task._id === obj.task_id;
+    //   });
+    // });
+    // return history;
+
+    return tasksAndSubmissions;
   }
 
-  async getUserTask(params) {
-    const getUserTask = await this.repo.findWhere({task_assignee: params.assignee_id});
+  async getAssigneeTask(params) {
+    const { organization_id, ...filter } = params;
 
-    return getUserTask;
+    const tasks = await this.getAllTasks(organization_id);
+
+    const getUserTask = await this.repo.findWhere(organization_id, {
+      ...filter,
+      sub_collection: sub_collections.assigned_tasks,
+    });
+
+    const assignedTask = getUserTask.data?.data;
+
+    const final = [];
+
+    assignedTask.forEach((assg) => {
+      const task = tasks.find((task) => task._id === assg.task_id);
+
+      final.push({
+        ...assg,
+        task,
+      });
+    });
+
+    return final;
   }
 
   async deleteTaskCategory(params) {
-    const deleteTask = await this.repo.delete({object_id: params.category_id});
+    const payload = { ...params, bulk_delete: false, filter: { sub_collection: sub_collections.categories } };
+
+    const deleteTask = await this.repo.delete(payload);
 
     return deleteTask;
   }
 
   async submitTask(data) {
-    const collectionName = 'submissions';
-    this.request = { ...this.request, collection_name: collectionName };
-    const submission = await this.repo.create(data);
+    const { organization_id, ...required } = data;
+    const submission = await this.repo.create({
+      organization_id,
+      ...required,
+      sub_collection: sub_collections.submissions,
+    });
     return submission;
   }
 }
